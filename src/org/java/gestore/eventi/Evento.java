68368c92220065f98da8a410a133cd9275a67c26
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-17 09:53:18 +0200

    first commit

---
+++ src/org/java/gestore/eventi/Evento.java
@@ -0,0 +1,19 @@
+package org.java.gestore.eventi;
+
+import java.security.PublicKey;
+
+public class Evento {
+		
+		private String titolo;
+		private String data;
+		private int postiTotali;
+		private int postiPrenotati;
+		
+		public Evento (String titolo, String data, int postiTotali, int postiTerminati) {
+			
+		}
+		
+		
+		
+
+}
